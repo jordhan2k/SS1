19fb6f34bb4dacfdd68866f973984f2c2271853d
Jordan Han <dunghoang1c.hanu@gmail.com>
2020-09-23 08:16:52 +0700

    Add practice B w2

---
+++ Week02/PracticeWeek02_B.py
@@ -0,0 +1,38 @@
+
+# Function to convert an English sentence to morse code
+#
+#
+#
+
+morse_code_dic = {
+            'A': '.-', 'B': '-...',
+            'C': '-.-.', 'D': '-..', 'E': '.',
+            'F': '..-.', 'G': '--.', 'H': '....',
+            'I': '..', 'J': '.---', 'K': '-.-',
+            'L': '.-..', 'M': '--', 'N': '-.',
+            'O': '---', 'P': '.--.', 'Q': '--.-',
+            'R': '.-.', 'S': '...', 'T': '-',
+            'U': '..-', 'V': '...-', 'W': '.--',
+            'X': '-..-', 'Y': '-.--', 'Z': '--..',
+            '1': '.----', '2': '..---', '3': '...--',
+            '4': '....-', '5': '.....', '6': '-....',
+            '7': '--...', '8': '---..', '9': '----.',
+            '0': '-----', ', ': '--..--', '.': '.-.-.-',
+            '?': '..--..'
+    }
+def morse_code_converter(s):
+    morse_code = ""
+    for x in s:
+        if x != " ":
+            morse_code += morse_code_dic[x] + " "
+        else:
+            morse_code += " "
+    return morse_code
+
+
+if __name__ == '__main__':
+
+
+    s = input("Enter a string: ")
+    morse_code_converter(s)
+
