2315522c49b22a37ccb303df060c4d1806e3ab05
Jordan Han <dunghoang1c.hanu@gmail.com>
2020-09-28 22:59:05 +0700

    Update haversine

---
+++ Week02/haversine_formula.py
@@ -0,0 +1,18 @@
+import math
+
+#distance between latitudes (m)
+DIS_BTW_LAT = 110900
+PI = math.pi
+# 1 sec/latitude(m)
+SEC_LATITUDE = 30.82
+# 1 min/latitude (m)
+MIN_LATITTUDE = 1849
+# 1 sec/ longtitude on the equator (m)
+SEC_LONG_EQU = 30.92
+# Earth's radius (km)
+EARTH_RADIUS = 6371
+
+print(PI)
+
+
+
